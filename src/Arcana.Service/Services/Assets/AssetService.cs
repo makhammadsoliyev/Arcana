e8557c87a8148d8b329d031e3d6ed6590dc69020
Umidjon <makhammadsoliyev@gmail.com>
2024-04-17 17:30:45 +0500

    Finish AssetService

---
+++ src/Arcana.Service/Services/Assets/AssetService.cs
@@ -0,0 +1,55 @@
+﻿using Arcana.DataAccess.UnitOfWorks;
+using Arcana.Domain.Entities.Commons;
+using Arcana.Service.Configurations;
+using Arcana.Service.Exceptions;
+using Arcana.Service.Helpers;
+using Microsoft.AspNetCore.Http;
+
+namespace Arcana.Service.Services.Assets;
+
+public class AssetService(IUnitOfWork unitOfWork) : IAssetService
+{
+    public async Task<Asset> DeleteAsync(long id)
+    {
+        var asset = await unitOfWork.Assets.SelectAsync(asset => asset.Id == id)
+            ?? throw new NotFoundException($"Asset is not found with this id={id}");
+
+        var deletedAsset = await unitOfWork.Assets.DeleteAsync(asset);
+        await unitOfWork.SaveAsync();
+
+        return deletedAsset;
+    }
+
+    public async Task<Asset> GetByIdAsync(long id)
+    {
+        var asset = await unitOfWork.Assets.SelectAsync(asset => asset.Id == id)
+            ?? throw new NotFoundException($"Asset is not found with this id={id}");
+
+        return asset;
+    }
+
+    public async Task<Asset> UploadAsync(IFormFile file, FileType type)
+    {
+        var directoryPath = Path.Combine(EnvironmentHelper.WebRootPath, nameof(type));
+        Directory.CreateDirectory(directoryPath);
+
+        var fullPath = Path.Combine(directoryPath, file.Name);
+
+        using var stream = new FileStream(fullPath, FileMode.Create);
+        await file.CopyToAsync(stream);
+        await stream.FlushAsync();
+        stream.Close();
+
+        var asset = new Asset()
+        {
+            Path = fullPath,
+            Name = file.Name,
+            CreatedByUserId = 1
+        };
+
+        var createdAsset = await unitOfWork.Assets.InsertAsync(asset);
+        await unitOfWork.SaveAsync();
+
+        return createdAsset;
+    }
+}
---
+++ src/Arcana.Service/Services/Assets/IAssetService.cs
@@ -0,0 +1,12 @@
+﻿using Arcana.Domain.Entities.Commons;
+using Arcana.Service.Configurations;
+using Microsoft.AspNetCore.Http;
+
+namespace Arcana.Service.Services.Assets;
+
+public interface IAssetService
+{
+    Task<Asset> UploadAsync(IFormFile file, FileType type);
+    Task<Asset> DeleteAsync(long id);
+    Task<Asset> GetByIdAsync(long id);
+}
