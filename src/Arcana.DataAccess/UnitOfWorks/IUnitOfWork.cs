b5fb39c97965223331a20d3303decc283685f478
Umidjon <makhammadsoliyev@gmail.com>
2024-04-17 15:35:21 +0500

    Add UnitOfWork

---
+++ src/Arcana.DataAccess/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using Arcana.DataAccess.Repositories;
+using Arcana.Domain.Entities.Commons;
+using Arcana.Domain.Entities.Instructors;
+using Arcana.Domain.Entities.Students;
+using Arcana.Domain.Entities.Users;
+
+namespace Arcana.DataAccess.UnitOfWorks;
+
+public interface IUnitOfWork : IDisposable
+{
+    IRepository<User> Users { get; }
+    IRepository<Asset> Assets { get; }
+    IRepository<Student> Students { get; }
+    IRepository<UserRole> UserRoles { get; }
+    IRepository<Instructor> Instructors { get; }
+    IRepository<Permission> Permissions { get; }
+    IRepository<UserPermission> UserPermissions { get; }
+
+    Task<bool> SaveAsync();
+}
---
+++ src/Arcana.DataAccess/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,47 @@
+﻿using Arcana.DataAccess.Context;
+using Arcana.DataAccess.Repositories;
+using Arcana.Domain.Entities.Commons;
+using Arcana.Domain.Entities.Instructors;
+using Arcana.Domain.Entities.Students;
+using Arcana.Domain.Entities.Users;
+
+namespace Arcana.DataAccess.UnitOfWorks;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly ArcanaDbContext _context;
+
+    public IRepository<User> Users { get; }
+
+    public IRepository<Asset> Assets { get; }
+
+    public IRepository<Student> Students { get; }
+
+    public IRepository<UserRole> UserRoles { get; }
+
+    public IRepository<Instructor> Instructors { get; }
+
+    public IRepository<Permission> Permissions { get; }
+
+    public IRepository<UserPermission> UserPermissions { get; }
+
+    public UnitOfWork(ArcanaDbContext context)
+    {
+        _context = context;
+        Users = new Repository<User>(_context);
+        Assets = new Repository<Asset>(_context);
+        Students = new Repository<Student>(_context);
+        UserRoles = new Repository<UserRole>(_context);
+        Instructors = new Repository<Instructor>(_context);
+        Permissions = new Repository<Permission>(_context);
+        UserPermissions = new Repository<UserPermission>(_context);
+    }
+
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+    }
+
+    public async Task<bool> SaveAsync()
+        => await _context.SaveChangesAsync() > 0;
+}
